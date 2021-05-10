509c6a286e836cea5d5eef4d49f220a76c88da2e
Jeremie Klinger <jeremie@mbp-de-jeremie.home>
2021-05-10 20:37:17 +0200

    test file added

---
+++ tests/DiscreteWalker_test.py
@@ -0,0 +1,19 @@
+### Just another python script
+
+import numpy as np
+import sys
+import time
+
+import DiscreteWalker.build.module_DiscreteWalker as md
+
+if __name__=='__main__':
+
+    beta=0.7
+    xmax=100
+    nb_simus=100000
+    walker=md.SATWWalker(beta)
+    walker.set_seed(int(time.time()+100000000))
+    walker.print_details()
+
+    result=walker.max_prob(1,xmax,nb_simus)
+    print("prob : {0:e}".format(result))
