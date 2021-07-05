20dd04d4e1a0a362e21ae4dc1a056b06bee6317e
jeremie klinger <jeremie.klinger@gmail.com>
2021-07-05 16:16:37 +0200

    various small changes

--- d_dimDiscreteWalker/test.py
+++ d_dimDiscreteWalker/test.py
@@ -1,10 +1,11 @@
 import numpy as np
+import sys
 import build.module_d_dimDiscreteWalker as md
 
 walker=md.DiscreteWalker("lala",3,[0,0,0],np.random.randint(100000))
 
 walker.print_details()
-for k in range(10):
-    walker.move(1)
+for k in range(int(sys.argv[1])):
+    walker.move(0)
 
 walker.print_details()
