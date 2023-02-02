41d4671009adda3395530d6cfad83929864e7801
jeremie klinger <jeremie.klinger@gmail.com>
2023-02-02 10:24:27 +0100

    hf

--- isotropicWalker/src/observables/Observables.cpp
+++ isotropicWalker/src/observables/Observables.cpp
@@ -282,6 +282,7 @@ py::list fpt_distribution_eccentric_inner_disk_outer_reflecting(
   py::list ret = py::cast(result_tmp);
   return ret;
 }
+
 double kac_flux_eccentric_inner_disk_outer_reflecting(
     const double R_int, const double R_out, double const offset,
     GaussianWalker &walker, const int angle_subdivisions, const int n) {
@@ -352,17 +353,19 @@ py::list fpt_distribution_eccentric_inner_disk_outer_reflecting_any_position(
 
     // prepare for the run
 
+    int d = walker.get_dimension();
     walker.set_lifetime(0);
     walker.set_coord(0, r0 * cos(theta0));
     walker.set_coord(1, r0 * sin(theta0));
+    for (int k = 2; k < d; k++) {
+      walker.set_coord(k, r0 * sin(theta0));
+    }
 
     // initialize vector center --> only works in 2D
-    vector<double> inner_centre(2);
+    vector<double> inner_centre(d);
     inner_centre[0] = -offset;
-    inner_centre[1] = 0.0;
-    int d = walker.get_dimension();
-    for (int k = 2; k < d; k++) {
-      walker.set_coord(k, 0.0);
+    for (int k = 1; k < d; k++) {
+      inner_centre[k] = 0.0;
     }
 
     double counter = 0;
@@ -374,8 +377,9 @@ py::list fpt_distribution_eccentric_inner_disk_outer_reflecting_any_position(
       walker.move();
       double r = walker.get_radial_dist();
       if (r > R_out) {
-        walker.set_coord(0, (2 * R_out - r) / r * walker.get_pos()[0]);
-        walker.set_coord(1, (2 * R_out - r) / r * walker.get_pos()[1]);
+        for (int k = 0; k < d; k++) {
+          walker.set_coord(k, (2 * R_out - r) / r * walker.get_pos()[k]);
+        }
       }
     }
     result_tmp[i] = counter;
